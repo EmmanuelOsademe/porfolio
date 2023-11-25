bb46522afad2096c7a5b797113a96bed8a7dea23
Emmanuel Osademe <emma.osademe@gmail.com>
2023-11-25 22:57:10 +0100

    added profile picture

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,43 +1,74 @@
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 import { styles } from "../style";
-import {ComputerCanvas} from "./canvas"
+import { ComputerCanvas } from "./canvas";
 
 const Hero: React.FC = () => {
-
-    return (
-        <section className="relative w-full h-screen mx-auto">
-            <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
-                <div className="flex flex-col justify-center items-center mt-5">
-                    <div className="w-5 h-5 rounded-full bg-[#915eff]"/>
-                    <div  className="w-1 sm:h-80 h-40 violet-gradient"/>
-                </div>
-                <div className="">
-                    <h1 className={`${styles.heroHeadText}`}>Hi, I'm <span className="text-[#915eff]">Emmanuel</span></h1>
-                    <p className={`${styles.heroSubText} mt-2`}>
-                        I develop full stack web applications, <br className="sm:block hidden"/>user interfaces, and backend projects
-                    </p>
-                </div>
+  return (
+    <section className="relative w-full h-screen mx-auto">
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[100px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+      >
+        <div className="w-full flex flex-col-reverse lg:flex-row md:justify-between md:items-start z-10">
+          <div className="flex gap-4 items-start">
+            <div className="flex flex-col justify-center items-center mt-5">
+              <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+              <div className="w-1 sm:h-56 h-40 violet-gradient" />
             </div>
-            <ComputerCanvas />
-            <div className="absolute xs:bottom-10 bottom-12 w-full flex justify-center items-center">
-                <a href="#about">
-                    <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
-                        <motion.div
-                            animate={{
-                                y: [0, 24, 0],
-                            }}
-                            transition={{
-                                duration: 1.5,
-                                repeat: Infinity,
-                                repeatType: "loop"
-                            }}
-                            className="w-3 h-3 rounded-full bg-secondary mb-1"
-                        />
-                    </div>
+            <div className="flex flex-col gap-2">
+              <h1 className={`${styles.heroHeadText}`}>
+                Hi, I'm <span className="text-[#915eff]">Emmanuel</span>
+              </h1>
+              <p className={`${styles.heroSubText} mt-2`}>
+                I develop full stack web applications,{" "}
+                <br className="sm:block hidden" />
+                user interfaces, and backend projects
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col max-lg:flex-row gap-2">
+            <img
+              src="/Profile.jpeg"
+              alt="profile"
+              className="w-40 h-40 rounded-full"
+            />
+            <div className="flex flex-col justify-center items-start sm:items-center">
+              <div className="text-white text-base cursor-pointer hover:text-[#915eff] ">
+                <a href="https://github.com/EmmanuelOsademe" target="_blank">
+                  See Github Page
                 </a>
+              </div>
+              <div className="text-white text-base cursor-pointer hover:text-[#915eff]">
+                <a
+                  href="https://www.linkedin.com/in/emmanuel-osademe/"
+                  target="_blank"
+                >
+                  See LinkedIn Page
+                </a>
+              </div>
             </div>
-        </section>
-    )
-}
+          </div>
+        </div>
+      </div>
+      <ComputerCanvas />
+      <div className="absolute xs:bottom-10 bottom-12 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
+    </section>
+  );
+};
 
 export default Hero;
--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -1,5 +1,5 @@
-import { Tilt } from "react-tilt";
-import {motion} from "framer-motion";
+// import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
 import { github, website } from "../assets";
 import { styles } from "../style";
 import { SectionWrapper } from "../hoc";
@@ -7,99 +7,88 @@ import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
 interface CardProps {
-    index: number,
-    project: typeof projects[0]
+  index: number;
+  project: (typeof projects)[0];
 }
 
-const ProjectCard: React.FC<CardProps> = ({index, project}) => {
-
-    return (
-        <motion.div
-            variants={fadeIn("up", "spring", index * 0.5, 0.75)}
-            className=""
-        >
-            <Tilt 
-                options={{
-                    max: 45,
-                    scale: 1,
-                    speed: 450
-                }}
-                className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
-            >
-                <div className="relative w-full h-[230px]">
-                    <img 
-                        src={project.image}
-                        alt={project.name}
-                        className="w-full h-full fill rounded-2xl"
-                    />
-                    <div className="absolute inset-0 flex justify-end m-[2px] gap-1 card-img_hover">
-                        <div 
-                            className="black-gradient w-[24px] h-[24px] rounded-full flex justify-center items-center cursor-pointer"
-                            onClick={() => window.open(project.source_code_link, "_blank")}
-                        >
-                            <img 
-                                src={github}
-                                alt="github link"
-                                className="w-full h-full object-contain"
-                            />
-                        </div>
-                        {project.deployment_link && (
-                            <div 
-                                className="black-gradient w-6 h-6 rounded-full flex justify-center items-center cursor-pointer"
-                                onClick={() => window.open(project.deployment_link, "_blank")}
-                            >
-                                <img 
-                                    src={website}
-                                    alt="github link"
-                                    className="w-full h-full rounded-full bg-white object-contain"
-                                />
-                            </div>
-                        )}
-                    </div>
-                </div>
-                <div className="mt-5">
-                    <h3 className="text-white font-bold text-[24px]">{project.name}</h3>
-                    <p className="mt-2 text-secondary text-[14px]">{project.description}</p>
-                </div>
-                <div className="mt-4 flex flex-wrap gap-2">
-                    {project.tags.map((tag) => (
-                        <p key={tag.name} className={`text-[14px] ${tag.color}`}>#{tag.name}</p>
-                    ))}
-                </div>
-            </Tilt>
-        </motion.div>
-    )
-}
+const ProjectCard: React.FC<CardProps> = ({ index, project }) => {
+  return (
+    <motion.div
+      variants={fadeIn("up", "spring", index * 0.5, 0.75)}
+      className=""
+    >
+      <div className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full">
+        <div className="relative w-full h-[270px]">
+          <img
+            src={project.image}
+            alt={project.name}
+            className="w-full h-full object-fit rounded-2xl"
+          />
+        </div>
+        <div className="mt-5">
+          <h3 className="text-white font-bold text-[24px]">{project.name}</h3>
+          <p className="mt-2 text-secondary text-[14px]">
+            {project.description}
+          </p>
+        </div>
+        <div className=" mt-4 flex flex-row justify-between">
+          <a
+            href={project.deployment_link}
+            className=" text-sm cursor-pointer text-purple-200 hover:text-purple-400"
+            target="_blank"
+          >
+            Visit Live site
+          </a>
+          <a
+            href={project.source_code_link}
+            className=" text-sm cursor-pointer text-purple-200 hover:text-purple-400"
+            target="_blank"
+          >
+            See project on Github
+          </a>
+        </div>
+        <div className="mt-4 flex flex-wrap gap-2">
+          {project.tags.map((tag) => (
+            <p key={tag.name} className={`text-[14px] ${tag.color}`}>
+              #{tag.name}
+            </p>
+          ))}
+        </div>
+      </div>
+    </motion.div>
+  );
+};
 
 const Works: React.FC = () => {
-
-    return (
-        <>
-            <motion.div
-                variants={textVariant()}
-            >
-                <p className={`${styles.sectionSubText}`}>My Projects</p>
-                <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
-            </motion.div>
-            <div className="w-full flex">
-                <motion.p
-                    variants={fadeIn("", "", 0.1, 1)}
-                    className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
-                >
-                    The following projects showcases my skills and experience through
-                    real-world examples of my work. Each project is briefly described with
-                    links to code repositories and live demos. It reflects my
-                    ability to solve complex problems, work with different technologies,
-                    and manage projects effectively.
-                </motion.p>
-            </div>
-            <div className="mt-20 flex flex-wrap justify-center gap-7">
-                {projects.map((project, index) => (
-                    <ProjectCard key={`project-${index}`} index={index} project={project}/>
-                ))}
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText}`}>My Projects</p>
+        <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
+      </motion.div>
+      <div className="w-full flex">
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
+          The following projects showcases my skills and experience through
+          real-world examples of my work. Each project is briefly described with
+          links to code repositories and live demos. It reflects my ability to
+          solve complex problems, work with different technologies, and manage
+          projects effectively.
+        </motion.p>
+      </div>
+      <div className="mt-20 flex flex-wrap justify-center gap-7">
+        {projects.map((project, index) => (
+          <ProjectCard
+            key={`project-${index}`}
+            index={index}
+            project={project}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default SectionWrapper(Works, "work");
